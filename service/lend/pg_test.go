e3f85e1081ad2bd8d4feb747f31c9869ca38a4df
minhkhiemm <vominhkhiem212@gmail.com>
2018-08-06 10:21:07 +0700

    test for validation lend book

--- service/lend/pg_test.go
+++ service/lend/pg_test.go
@@ -1,49 +1,56 @@
-package category
+package lend
 
 import (
 	"context"
-	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/jinzhu/gorm"
-
-	testutil "github.com/minhkhiemm/example-go/config/database/pg/util"
 	"github.com/minhkhiemm/example-go/domain"
 )
 
-func Test_pgService_Create(t *testing.T) {
-	t.Parallel()
-	testDB, _, cleanup := testutil.CreateTestDatabase(t)
-	defer cleanup()
-	err := testutil.MigrateTables(testDB)
-	if err != nil {
-		t.Fatalf("Failed to migrate table by error %v", err)
-	}
-
+func TestNewPGService(t *testing.T) {
 	type args struct {
-		p *domain.Category
+		db *gorm.DB
+	}
+	tests := []struct {
+		name string
+		args args
+		want Service
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewPGService(tt.args.db); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPGService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_pgService_Create(t *testing.T) {
+	type fields struct {
+		db *gorm.DB
+	}
+	type args struct {
+		in0 context.Context
+		p   *domain.Lend
 	}
 	tests := []struct {
 		name    string
+		fields  fields
 		args    args
 		wantErr bool
 	}{
-		{
-			name: "Success",
-			args: args{
-				&domain.Category{
-					Name: "Create category 1",
-				},
-			},
-		},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &pgService{
-				db: testDB,
+				db: tt.fields.db,
 			}
-			if err := s.Create(context.Background(), tt.args.p); (err != nil) != tt.wantErr {
+			if err := s.Create(tt.args.in0, tt.args.p); (err != nil) != tt.wantErr {
 				t.Errorf("pgService.Create() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -51,70 +58,34 @@ func Test_pgService_Create(t *testing.T) {
 }
 
 func Test_pgService_Update(t *testing.T) {
-	t.Parallel()
-	testDB, _, cleanup := testutil.CreateTestDatabase(t)
-	defer cleanup()
-	err := testutil.MigrateTables(testDB)
-	if err != nil {
-		t.Fatalf("Failed to migrate table by error %v", err)
+	type fields struct {
+		db *gorm.DB
 	}
-
-	category := domain.Category{}
-	err = testDB.Create(&category).Error
-	if err != nil {
-		t.Fatalf("Failed to create category by error %v", err)
-	}
-
-	fakeCategoryID := domain.MustGetUUIDFromString("1698bbd6-e0c8-4957-a5a9-8c536970994b")
-
 	type args struct {
-		p *domain.Category
+		in0 context.Context
+		p   *domain.Lend
 	}
 	tests := []struct {
 		name    string
+		fields  fields
 		args    args
-		wantErr error
+		want    *domain.Lend
+		wantErr bool
 	}{
-		{
-			name: "Update success",
-			args: args{
-				&domain.Category{
-					Model: domain.Model{ID: category.ID},
-					Name:  "category name 1",
-				},
-			},
-		},
-		{
-			name: "failed update",
-			args: args{
-				&domain.Category{
-					Model: domain.Model{ID: fakeCategoryID},
-					Name:  "category name 2",
-				},
-			},
-			wantErr: ErrNotFound,
-		},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &pgService{
-				db: testDB,
+				db: tt.fields.db,
 			}
-			_, err := s.Update(context.Background(), tt.args.p)
-			fmt.Println("err", err)
-			fmt.Println("wantErr", tt.wantErr)
-			if err == tt.wantErr {
-				t.Errorf("test for equal error is %v, want Error %v", err, tt.wantErr)
-			}
-			if err != tt.wantErr {
-				t.Errorf("error is %v, want Error %v", err, tt.wantErr)
-			}
-			if err != nil {
+			got, err := s.Update(tt.args.in0, tt.args.p)
+			if (err != nil) != tt.wantErr {
 				t.Errorf("pgService.Update() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if err == nil && tt.wantErr != nil {
-				t.Errorf("pgService.Update() error = %v, wantErr %v", err, tt.wantErr)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pgService.Update() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -126,13 +97,13 @@ func Test_pgService_Find(t *testing.T) {
 	}
 	type args struct {
 		in0 context.Context
-		p   *domain.Category
+		p   *domain.Lend
 	}
 	tests := []struct {
 		name    string
 		fields  fields
 		args    args
-		want    *domain.Category
+		want    *domain.Lend
 		wantErr bool
 	}{
 		// TODO: Add test cases.
@@ -165,7 +136,7 @@ func Test_pgService_FindAll(t *testing.T) {
 		name    string
 		fields  fields
 		args    args
-		want    []domain.Category
+		want    []domain.Lend
 		wantErr bool
 	}{
 		// TODO: Add test cases.
@@ -193,7 +164,7 @@ func Test_pgService_Delete(t *testing.T) {
 	}
 	type args struct {
 		in0 context.Context
-		p   *domain.Category
+		p   *domain.Lend
 	}
 	tests := []struct {
 		name    string
